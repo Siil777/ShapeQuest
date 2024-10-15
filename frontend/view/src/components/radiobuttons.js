1f95e13d77f0b408ef30651286249c2e906cdd85
Siil777 <pavel.ivanov.thk@gmail.com>
2024-10-15 19:47:08 +0300

    feat: radio component

--- frontend/view/src/components/radiobuttons.js
+++ frontend/view/src/components/radiobuttons.js
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import Radio from '@mui/material/Radio';
 
-const RadioBtn = ({value,onChange,checked}) =>{
+const RadioBtn = ({value,onChange,checked,name}) =>{
     return(
         <div>
             <Radio 
             value={value}
             onChange={onChange}
             checked={checked}
+            name={name}
             color='primary'
             />
             {value}
